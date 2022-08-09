aeb30b341a0895802daf99351624abdb3e87006d
jtnovellis <jtnovellis88@gmail.com>
2022-08-08 21:20:26 -0500

    paso-5

---
+++ index.js
@@ -0,0 +1,78 @@
+let tasks = [];
+const inputTask = document.querySelector('.input-task');
+const addButton = document.querySelector('.add-button');
+const form = document.querySelector('#form');
+
+addButton.addEventListener('click', event => {
+  event.preventDefault();
+  if (inputTask.value !== '') {
+    handleSubmit(inputTask.value);
+    inputTask.value = ''
+    renderTask();
+  }
+});
+
+function handleSubmit(value) {
+  const newTask = {
+    id: (Math.random() * 100).toString(36).slice(3),
+    title: value,
+    completed: false
+  };
+  tasks.unshift(newTask);
+};
+
+function renderTask() {
+  const html = tasks.map(task => {
+    return `
+    <div class="each-task">
+      <input type="checkbox" name="checkbox" id="${task.id}">
+      <div class="task">${task.title}</div>
+    </div>
+    `;
+  });
+  const allTasks = document.querySelector('.all-tasks');
+  allTasks.innerHTML = html.join('');
+
+  const checkbox = document.querySelectorAll('input[name=checkbox]');
+  checkbox.forEach(check => {
+    check.addEventListener('change', event => {
+      event.preventDefault();
+      let proof = event.target.checked
+      if (proof) {
+        handleCheckTask(check.id, proof);
+        check.classList.add('is-completed');
+      } else {
+        handleCheckTask(check.id, proof);
+        check.classList.remove('is-completed');
+      };
+    });
+  });
+};
+
+function handleCheckTask(id, event) {
+  tasks.forEach(task => {
+    if (task.id === id && event === true) {
+      task.completed = true;
+    } else if (task.id === id && event === false) {
+      task.completed = false;
+    }
+  });
+};
+
+const deleteButton = document.querySelector('.delete-button');
+deleteButton.addEventListener('click', event => {
+  handleDelete();
+  renderTask();
+});
+
+function handleDelete() {
+  tasks.map(task => {
+    if (task.completed === true) {
+      const index = tasks.indexOf(task)
+      for (let i = 0; i < tasks.length; i++) {
+        if (i === index)
+        tasks.splice(index,1);
+      };
+    };
+  });
+};
